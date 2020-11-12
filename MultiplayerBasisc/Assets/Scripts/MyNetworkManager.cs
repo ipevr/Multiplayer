130417dcb904d628513e47ab06eac792d810270b
Ingo Peters <ingo.peters2@gmx.de>
2020-11-12 22:29:16 +0100

    - Own NetworkManager

---
+++ MultiplayerBasisc/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I connected to the server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log($"A player was added to the server. There are {numPlayers} Players on the server now.");
+    }
+}
